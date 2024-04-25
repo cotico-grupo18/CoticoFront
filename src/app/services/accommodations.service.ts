ea8d99d3eba21bbe060fecba3868ddbf13a5e4e3
majo425 <mariacardenasm@javeriana.edu.co>
2024-04-24 21:25:22 -0500

    Update accommodations.service.ts

--- src/app/services/accommodations.service.ts
+++ src/app/services/accommodations.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
-@Injectable({
+@Injectable({ //HOLAAA
   providedIn: 'root'
 })
 export class AccommodationsService {
