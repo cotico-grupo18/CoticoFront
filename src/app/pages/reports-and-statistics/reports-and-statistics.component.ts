9ebf9eb9d88323b051838ffbd6409e3dd407a304
majo425 <mariacardenasm@javeriana.edu.co>
2024-05-01 21:03:31 -0500

    Graficos

--- src/app/pages/reports-and-statistics/reports-and-statistics.component.ts
+++ src/app/pages/reports-and-statistics/reports-and-statistics.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-reports-and-statistics',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './reports-and-statistics.component.scss'
 })
 export class ReportsAndStatisticsComponent {
+  generarReporte(): void {
+    const divImagenObservaciones = document.getElementById("imagenObservaciones");
+    if (divImagenObservaciones) {
+      const imagen = document.getElementById("imagen") as HTMLImageElement;
+      if (imagen) {
+        imagen.src = "assets/img/grafico.png";
+      }
+
+      divImagenObservaciones.style.display = "block";
+    }
+  }
+
+  guardarObservaciones(): void {
+    Swal.fire({
+      icon: 'success',
+      title: '¡Observaciones guardadas!',
+      text: 'Se han guardado las observaciones correctamente.',
+    });
+  }
 
 }
